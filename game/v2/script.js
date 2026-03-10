909105803dc3d67bcd695071a01f3aa7a5bf11c3
Vicky Li <vyli@ucdavis.edu>
2026-03-10 15:49:44 -0700

    game on images

--- game/v2/script.js
+++ game/v2/script.js
@@ -3,21 +3,62 @@
     "use strict"; 
     console.log("reading js"); 
 
-    //start page 
-    const help = document.querySelector(".help");
-    const music = document.querySelector(".sound"); 
-    const back = document.querySelector(".return"); 
+    //basics 
+    const audio = new Audio("music/backgroundmusic.mp3"); 
+        audio.loop = true; 
 
-    help.addEventListener("click", function() {
-        document.querySelector("#overlay").classList.remove("hidden");
+    const start = document.querySelector("#startbutton"); 
 
-        back.addEventListener("click", function() {
-            document.querySelector("#overlay").className = "hidden"; 
+    //repeating buttons 
+    const help = document.querySelectorAll(".help");
+    const music = document.querySelectorAll(".sound"); 
+    const overlayback = document.querySelector("#overlayreturn"); 
+    const sourcesback = document.querySelector("#sourcesreturn"); 
+    const quit = document.querySelectorAll(".quit"); 
+    const sources = document.querySelectorAll(".sourcesbutton");
+    
+    help.forEach(function(button) {
+        button.addEventListener("click", function() {
+            document.querySelector("#overlay").classList.remove("hidden");
         }); 
     }); 
 
-    document.querySelector("#startbutton").addEventListener("click", function() {
+    overlayback.addEventListener("click", function() {
+        document.querySelector("#overlay").className = "hidden"; 
+    }); 
+
+    music.forEach(function(button) {
+        button.addEventListener("click", function() {
+            if (audio.paused) {
+                audio.play(); 
+            } else 
+                audio.pause(); 
+        }); 
+    }); 
+
+    sources.forEach(function(button) {
+        button.addEventListener("click", function() {
+            document.querySelector("#sourcespage").classList.remove("hidden");
+        }); 
+    }); 
+
+    sourcesback.addEventListener("click", function() {
+        document.querySelector("#sourcespage").className = "hidden"; 
+    }); 
+
+    quit.forEach(function(button) {
+        button.addEventListener("click", function() {
+            location.reload(); 
+        }); 
+    }); 
+    
+    start.addEventListener("click", function() {
         document.querySelector("#start").className = "hidden"; 
         document.querySelector("#game").classList.remove("hidden");
     }); 
+
+    
+
+    
+
 }) (); 
